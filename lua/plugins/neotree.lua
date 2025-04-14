3dc8849cc85fc960a8cec09274e2af5a49fcecde
Sam Fox <safox002@gmail.com>
2025-04-13 23:12:58 -0400

    Hide dotfile in neotree by default

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -26,10 +26,10 @@ return {
         }
       end,
       keys = {
-        { '\\',          ':Neotree reveal<CR>',               desc = 'NeoTree reveal', silent = true },
-        { '<leader>w',   ':Neotree toggle float<CR>',         silent = true,           desc = 'Float File Explorer' },
-        { '<leader>e',   ':Neotree toggle position=left<CR>', silent = true,           desc = 'Left File Explorer' },
-        { '<leader>ngs', ':Neotree float git_status<CR>',     silent = true,           desc = 'Neotree Open Git Status Window' },
+        { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+        { '<leader>w', ':Neotree toggle float<CR>', silent = true, desc = 'Float File Explorer' },
+        { '<leader>e', ':Neotree toggle position=left<CR>', silent = true, desc = 'Left File Explorer' },
+        { '<leader>ngs', ':Neotree float git_status<CR>', silent = true, desc = 'Neotree Open Git Status Window' },
       },
     },
   },
@@ -45,10 +45,10 @@ return {
       popup_border_style = 'rounded',
       enable_git_status = true,
       enable_diagnostics = true,
-      enable_normal_mode_for_inputs = true,                              -- Enable normal mode for input dialogs.
+      enable_normal_mode_for_inputs = true, -- Enable normal mode for input dialogs.
       open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' }, -- when opening files, do not use windows containing these filetypes or buftypes
-      sort_case_insensitive = false,                                     -- used when sorting files and directories in the tree
-      sort_function = nil,                                               -- use a custom function for sorting files and directories in the tree
+      sort_case_insensitive = false, -- used when sorting files and directories in the tree
+      sort_function = nil, -- use a custom function for sorting files and directories in the tree
       -- sort_function = function (a,b)
       --       if a.type == b.type then
       --           return a.path > b.path
@@ -196,7 +196,7 @@ return {
       filesystem = {
         filtered_items = {
           visible = false, -- when true, they will just be displayed differently than normal items
-          hide_dotfiles = false,
+          -- hide_dotfiles = false,
           hide_gitignored = false,
           hide_hidden = false, -- only works on Windows for hidden files/directories
           hide_by_name = {
@@ -225,11 +225,11 @@ return {
           },
         },
         follow_current_file = {
-          enabled = false,                      -- This will find and focus the file in the active buffer every time
+          enabled = false, -- This will find and focus the file in the active buffer every time
           --               -- the current file is changed while the tree is open.
-          leave_dirs_open = false,              -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+          leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
         },
-        group_empty_dirs = false,               -- when true, empty folders will be grouped together
+        group_empty_dirs = false, -- when true, empty folders will be grouped together
         hijack_netrw_behavior = 'open_default', -- netrw disabled, opening a directory opens neo-tree
         -- in whatever position is specified in window.position
         -- "open_current",  -- netrw disabled, opening a directory opens within the
@@ -272,11 +272,11 @@ return {
       },
       buffers = {
         follow_current_file = {
-          enabled = true,          -- This will find and focus the file in the active buffer every time
+          enabled = true, -- This will find and focus the file in the active buffer every time
           --              -- the current file is changed while the tree is open.
           leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
         },
-        group_empty_dirs = true,   -- when true, empty folders will be grouped together
+        group_empty_dirs = true, -- when true, empty folders will be grouped together
         show_unloaded = true,
         window = {
           mappings = {
